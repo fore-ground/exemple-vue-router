1571a39bf082bfd2138ae9c2c0d3822bf83a0628
@Sombre-Osmoze <sombre@osmoze.xyz>
2023-04-01 20:52:47 +0000

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
-<template class="bg-red-500">
-  <div class="bg-gray-900">
+<template>
+  <div class="h-screen bg-gray-900">
     <p class="text-white">some text</p>
   </div>
 </template>
